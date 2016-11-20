b86a76a3209ae07e709ed732bfda26f0905827e6
Pierre Chabiland <p.chabiland@croquepixel.net>
2016-11-20 09:47:34 +0100

    merge

--- chrome-extension/src/index.js
+++ chrome-extension/src/index.js
@@ -3,27 +3,27 @@ console.log(location.pathname);
 const toolbar = document.querySelector('#leftToolbar');
 console.log(toolbar);
 const toolbarGrp = document.createElement("div");
-toolbarGrp.setAttribute('class','gc-tool nav nav-pills nav-stacked pull-left persoToolbar');
+toolbarGrp.setAttribute('class','gg-tool nav nav-pills nav-stacked pull-left persoToolbar');
 toolbarGrp.innerHTML = `
     <div class="toolbarGroup">
-        <a class="gc-icon js-open-gc-quests white" href="#">
-            GC
+        <a class="gg-icon js-open-gg-quests white" href="#">
+            GG
         </a> 
     </div>
 `;
 document.body.insertBefore(toolbarGrp,toolbar);
 
 const quests = document.createElement("div");
-quests.setAttribute('class','gc-quests gc-hide');
+quests.setAttribute('class','gg-quests gg-hide');
 quests.innerHTML = `
     Pouet
 `;
 document.body.insertBefore(quests,toolbar);
 //js-gc-quests
 
-document.querySelector('.js-open-gc-quests').addEventListener('click',(event)=>{
+document.querySelector('.js-open-gg-quests').addEventListener('click',(event)=>{
     console.log("pouet");
-    quests.classList.toggle('gc-hide');
+    quests.classList.toggle('gg-hide');
 });
 
 const pathnameRE = /^\/ark:\/[0-9]\/[a-z0-9]/; 
